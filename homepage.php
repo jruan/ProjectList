b9a205c044b6793ea98daf38ddac9b0a58077600
Jason <jruan@ucsd.edu>
2015-04-28 13:39:06 -0400

    finished landingpage, beginning to work on home page

---
+++ homepage.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta name = "viewport" content = "width = device-width; height = device-height; initial-scale = 1.0">
+		<meta charset = "UTF-8">
+		<link rel = "stylesheet" type = "text/css" href = "/css/header_css.css">
+		<title> Home </title>
+	</head>
+
+	<body>
+		<div id = "wrapper">
+			<div id = "header">
+				<div id = "nav">
+					<ul>
+						<li><a href = "#">Home </a></li>
+					</ul>
+				</div>
+			</div>
+		</div>	
+	</body>
+</html>
